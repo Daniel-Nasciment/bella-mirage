c5f793c8d4cfb15e5c4ba459446d4feef5fd7568
Daniel-Nasciment <daniel.fecaf@outlook.com>
2022-05-21 16:30:13 -0300

    Base project

---
+++ src/main/java/br/com/bm/BellaMirageApplication.java
@@ -0,0 +1,13 @@
+package br.com.bm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BellaMirageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BellaMirageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bm/BellaMirageApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BellaMirageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
